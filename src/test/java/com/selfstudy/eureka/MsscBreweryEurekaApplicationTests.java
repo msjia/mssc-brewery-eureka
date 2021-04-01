e6ec2f11ed9fa7087d17afd78a2b1905ab83d182
Michael <msjia@me.com>
2021-04-02 00:07:41 +0800

    initial project setup

---
+++ src/test/java/com/selfstudy/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfstudy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
